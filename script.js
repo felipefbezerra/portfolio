729fc380e5eb8203d7534ff0667a0e8cbd8091bd
Felipe Florentino Bezerra <contato.felipefbezerra@gmail.com>
2025-01-08 19:39:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-let menuIcon = document.querySelector('menu-icon');
+let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
-
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
+    menuIcon.classList.toggle('bx-x'); 
+    navbar.classList.toggle('active'); 
+};
 
-let sections  = document.querySelectorAll('section');
+let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
+window.onscroll = () => {
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+    sections.forEach((sec) => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100; 
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach((link) => {
+                link.classList.remove('active');
+            });
+            document
+                .querySelector(`header nav a[href="#${id}"]`)
+                .classList.add('active');
+        }
+    });
+};
